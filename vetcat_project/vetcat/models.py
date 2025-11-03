efaa1aa9e6a22058fcdfd76c3da3f3631412e9d0
Milk <145270243+Milky47Way@users.noreply.github.com>
2025-11-03 19:26:12 +0200

    Add files via upload

--- vetcat_project/vetcat/models.py
+++ vetcat_project/vetcat/models.py
@@ -7,6 +7,11 @@ class Breed(models.Model):
     breed_name = models.CharField(max_length=100)
     coat_color = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ['breed_name']
+        verbose_lst_name = 'Breed'
+        verbose_lst_name_plural = 'Breeds'
+
     def __str__(self):
         return f"{self.breed_name} {self.coat_color}"
 
@@ -16,6 +21,12 @@ class Owner(models.Model):
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=16)
     date_of_birth = models.DateField()
+
+    class Meta:
+        ordering = ['last_name']
+        verbose_lst_name = 'Owner'
+        verbose_lst_name_plural = 'Owners'
+
 
     def __str__(self):
         return f"{self.last_name} {self.first_name}  {self.phone_number} {self.date_of_birth}"
@@ -29,6 +40,11 @@ class Cat(models.Model):
     breed_name = models.ForeignKey(Breed, on_delete=models.SET_NULL, null=True, blank=True, related_name='cats')
     owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, null=True, blank=True, related_name='cats')
 
+    class Meta:
+        ordering = ['name']
+        verbose_lst_name = 'Cat'
+        verbose_lst_name_plural = 'Cats'
+
     def __str__(self):
         return f"{self.name} {self.age} {self.date_of_birth} {self.breed_name} {self.owner}"
 
@@ -40,4 +56,8 @@ class Cat(models.Model):
 #Breed.objects.all()
 #Owner.objects.all()
 #Cat.objects.all()
+
+
+
+
 
