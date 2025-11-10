37a65f670c29715945e456095f9569ed83c3881f
Milk <145270243+Milky47Way@users.noreply.github.com>
2025-11-10 18:49:48 +0200

    Modify home view to use render method
    Updated views to include render for home view.

--- vetcat_project/vetcat/views.py
+++ vetcat_project/vetcat/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import import render
 from models import Breed
 # Create your views here.
 
 def home(request):
     return HttpResponse ("Welcome!")
+    return render(request, 'home.html')
 
 def breeds(request):
     breeds_obj = Breed.objects.all()
@@ -13,4 +15,5 @@ def breeds(request):
 def breed(request, pk):
     breed_obj = Breed.objects.filter(id=pk)
     return HttpResponse(breed_obj)
+
 
